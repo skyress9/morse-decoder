7802bfac4938bf45694d8cd24ad981bb3041e39b
skyress9 <123456789example@gmail.com>
2021-08-17 16:34:38 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let arr = [];
+    let str = '';
+
+    for (let i = 0; i < expr.length; i++) {
+        str = str + expr[i];
+        if ((i + 1) % 10 === 0) {
+            arr.push(str);
+            str = '';
+        }
+    }
+
+    result = arr.map(e => {
+        let morse = '';
+        let str = '';
+        let difStr = '';
+        let dec = '';
+        e = Number(e);
+
+        while (e) {
+            str = (e % 10) + str;
+            e = Math.floor(e / 10)
+        }
+
+        for (let i = 0; i < str.length; i++) {
+            difStr = difStr + str[i];
+            if (difStr.length % 2 === 0) {
+                if (Number(difStr) === 10) {
+                    morse = morse + '.'
+                } else if (Number(difStr) === 11) {
+                    morse = morse + '-';
+                }
+                difStr = '';
+            }
+        }
+        
+        dec = MORSE_TABLE[morse];
+        return dec;
+    })
+
+    result = result.map(e => {
+        if (e == undefined) {
+            e = ' ';
+        }
+        return e;
+    })
+    return result.join('');
 }
 
 module.exports = {
